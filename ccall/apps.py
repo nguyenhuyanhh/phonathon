a35c4b8bc332c2cb17542892ff7a7fd88ffc501e
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2018-01-19 17:15:01 +0800

    Verbose name for ccall

--- ccall/apps.py
+++ ccall/apps.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+"""App configuration for ccall app."""
+
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
 
 class CcallConfig(AppConfig):
+    """App configuration for ccall app."""
     name = 'ccall'
+    verbose_name = 'Phonathon'
