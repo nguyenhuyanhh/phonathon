6b5a7c82caa73e14ab98b8da78efa50e72df6558
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2018-01-24 21:17:32 +0800

    Tests for model Pool

--- ccall/tests.py
+++ ccall/tests.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import Group, Permission
 from django.test import TestCase
 from django.urls import resolve
 
-from .models import Fund, PhonathonUser, Pledge, Prospect
+from .models import Fund, PhonathonUser, Pledge, Pool, Prospect
 from .views import home
 
 
@@ -175,6 +175,15 @@ class TestPledge(TestCase):
             pledge_amount=50, pledge_fund=test_fund, prospect=test_prospect)
         self.assertEqual(str(test_pledge),
                          'Alex Ang (S1234567A) - $50 (NTU Bursaries)')
+
+
+class TestPool(TestCase):
+    """Tests for model Pool."""
+
+    def test_string_representation(self):
+        """Test the string representation."""
+        test_model = Pool(name='AQ2017')
+        self.assertEqual(str(test_model), 'AQ2017')
 
 
 class TestViews(TestCase):
