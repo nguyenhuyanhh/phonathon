2ca7734083f0912100bc68fcb1c1e167d8269d64
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2018-02-06 11:26:28 +0800

    Refactor tests.py into smaller tests under tests/

--- ccall/tests/test_groups.py
+++ ccall/tests/test_groups.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
-"""Tests for app ccall."""
+"""Tests for default groups and permissions."""
 
 from __future__ import unicode_literals
 
 from django.contrib.auth.models import Group, Permission
 from django.test import TestCase
-from django.urls import resolve
-
-from .models import (Call, Fund, PhonathonUser, Pledge,
-                     Pool, Prospect, ResultCode)
-from .views import home
 
 
 class TestGroups(TestCase):
@@ -139,116 +134,3 @@ class TestGroups(TestCase):
         self.assertIn(add, self.caller_perms)
         self.assertNotIn(change, self.caller_perms)
         self.assertNotIn(delete, self.caller_perms)
-
-
-class TestStaffStatus(TestCase):
-    """Tests for staff status."""
-
-    def test_caller_created(self):
-        """Test staff status for a newly created caller."""
-        test_model = PhonathonUser(username='AlexA', name='Alex Ang')
-        test_model.save()
-        test_model.groups.add(Group.objects.get(name='Callers'))
-        self.assertFalse(test_model.is_staff)
-
-    def test_supervisor_created(self):
-        """Test staff status for a newly created supervisor."""
-        test_model = PhonathonUser(username='AlexA', name='Alex Ang')
-        test_model.save()
-        test_model.groups.add(Group.objects.get(name='Supervisors'))
-        self.assertTrue(test_model.is_staff)
-
-    def test_manager_created(self):
-        """Test staff status for a newly created supervisor."""
-        test_model = PhonathonUser(username='AlexA', name='Alex Ang')
-        test_model.save()
-        test_model.groups.add(Group.objects.get(name='Managers'))
-        self.assertTrue(test_model.is_staff)
-
-    def test_change_status(self):
-        """Test for status change."""
-        test_model = PhonathonUser(username='AlexA', name='Alex Ang')
-        test_model.save()
-        test_model.groups.add(Group.objects.get(name='Managers'))
-        self.assertTrue(test_model.is_staff)
-        test_model.groups.remove(Group.objects.get(name='Managers'))
-        test_model.groups.add(Group.objects.get(name='Callers'))
-        self.assertFalse(test_model.is_staff)
-
-
-class TestInitialSuperuser(TestCase):
-    """Tests for the initial superuser."""
-
-    def test_initial_superuser(self):
-        """Tests for the initial superuser."""
-        superuser = PhonathonUser.objects.get(username='admin')
-        self.assertTrue(superuser.is_superuser)
-        self.assertTrue(superuser.is_staff)
-
-
-class TestInitialResultCodes(TestCase):
-    """Tests for the initial result codes."""
-
-    def test_initial_result_code_count(self):
-        """Tests for the number of initial result codes."""
-        self.assertEqual(len(ResultCode.objects.all()), 13)
-
-    def test_initial_result_code_type(self):
-        """Tests for the types of initial result codes."""
-        self.assertEqual(len(ResultCode.objects.filter(is_complete=False)), 2)
-        self.assertEqual(len(ResultCode.objects.filter(is_complete=True)), 11)
-
-
-class TestModels(TestCase):
-    """Tests for all the models in the project."""
-
-    def setUp(self):
-        self.test_user = PhonathonUser(username='AlexA', name='Alex Ang')
-        self.test_prospect = Prospect(name='Alex Ang', nric='S1234567A')
-        self.test_fund = Fund(name='NTU Bursaries')
-        self.test_pool = Pool(name='AQ2017')
-        self.test_result_code = ResultCode(result_code='Specified Pledge (EM)')
-
-    def test_string_user(self):
-        """Test the string representation for PhonathonUser."""
-        self.assertEqual(str(self.test_user), 'Alex Ang (AlexA)')
-
-    def test_string_prospect(self):
-        """Test the string representation for Prospect."""
-        self.assertEqual(str(self.test_prospect), 'Alex Ang (S1234567A)')
-
-    def test_string_fund(self):
-        """Test the string representation for Fund."""
-        self.assertEqual(str(self.test_fund), 'NTU Bursaries')
-
-    def test_string_pledge(self):
-        """Test the string representation for Pledge."""
-        test_pledge = Pledge(
-            pledge_amount=50,
-            pledge_fund=self.test_fund,
-            prospect=self.test_prospect)
-        self.assertEqual(str(test_pledge),
-                         'Alex Ang (S1234567A) - $50 (NTU Bursaries)')
-
-    def test_string_pool(self):
-        """Test the string representation for Pool."""
-        self.assertEqual(str(self.test_pool), 'AQ2017')
-
-    def test_string_result_code(self):
-        """Test the string representation for ResultCode."""
-        self.assertEqual(str(self.test_result_code), 'Specified Pledge (EM)')
-
-    def test_string_call(self):
-        """Test the string representation for Call."""
-        test_call = Call(caller=self.test_user, prospect=self.test_prospect)
-        self.assertEqual(
-            str(test_call), 'Alex Ang (S1234567A) - Alex Ang (AlexA)')
-
-
-class TestViews(TestCase):
-    """Tests for views."""
-
-    def test_view_home(self):
-        """Test whether empty url resolves to default view."""
-        view = resolve('/')
-        self.assertEqual(view.func, home)
---
+++ ccall/tests/test_initial_data.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""Tests for initial data created during data migration."""
+
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import Group
+from django.test import TestCase
+
+from ..models import PhonathonUser, ResultCode
+
+
+class TestStaffStatus(TestCase):
+    """Tests for staff status."""
+
+    def test_caller_created(self):
+        """Test staff status for a newly created caller."""
+        test_model = PhonathonUser(username='AlexA', name='Alex Ang')
+        test_model.save()
+        test_model.groups.add(Group.objects.get(name='Callers'))
+        self.assertFalse(test_model.is_staff)
+
+    def test_supervisor_created(self):
+        """Test staff status for a newly created supervisor."""
+        test_model = PhonathonUser(username='AlexA', name='Alex Ang')
+        test_model.save()
+        test_model.groups.add(Group.objects.get(name='Supervisors'))
+        self.assertTrue(test_model.is_staff)
+
+    def test_manager_created(self):
+        """Test staff status for a newly created supervisor."""
+        test_model = PhonathonUser(username='AlexA', name='Alex Ang')
+        test_model.save()
+        test_model.groups.add(Group.objects.get(name='Managers'))
+        self.assertTrue(test_model.is_staff)
+
+    def test_change_status(self):
+        """Test for status change."""
+        test_model = PhonathonUser(username='AlexA', name='Alex Ang')
+        test_model.save()
+        test_model.groups.add(Group.objects.get(name='Managers'))
+        self.assertTrue(test_model.is_staff)
+        test_model.groups.remove(Group.objects.get(name='Managers'))
+        test_model.groups.add(Group.objects.get(name='Callers'))
+        self.assertFalse(test_model.is_staff)
+
+
+class TestInitialSuperuser(TestCase):
+    """Tests for the initial superuser."""
+
+    def test_initial_superuser(self):
+        """Tests for the initial superuser."""
+        superuser = PhonathonUser.objects.get(username='admin')
+        self.assertTrue(superuser.is_superuser)
+        self.assertTrue(superuser.is_staff)
+
+
+class TestInitialResultCodes(TestCase):
+    """Tests for the initial result codes."""
+
+    def test_initial_result_code_count(self):
+        """Tests for the number of initial result codes."""
+        self.assertEqual(len(ResultCode.objects.all()), 13)
+
+    def test_initial_result_code_type(self):
+        """Tests for the types of initial result codes."""
+        self.assertEqual(len(ResultCode.objects.filter(is_complete=False)), 2)
+        self.assertEqual(len(ResultCode.objects.filter(is_complete=True)), 11)
