40c258078623df6a8030c8c5df93caaf7f31798c
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2018-02-07 15:33:01 +0800

    Base template with Bootstrap 4

--- ccall/views.py
+++ ccall/views.py
@@ -3,9 +3,9 @@
 
 from __future__ import unicode_literals
 
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
     """Default view for ccall."""
-    return HttpResponse('Hello, World!')
+    return render(request, 'ccall/base.html')
