c66d2274c27e29d13eedd5d8fc18363ec23f036b
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2018-02-21 08:54:24 +0800

    Update display, filter and search options; hide group perms from form for non-managers

--- ccall/admin.py
+++ ccall/admin.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
 from .models import (Call, Fund, PhonathonUser, Pledge, Pool, Project,
                      Prospect, ResultCode)
@@ -12,33 +13,45 @@ from .models import (Call, Fund, PhonathonUser, Pledge, Pool, Project,
 
 class PhonathonUserAdmin(UserAdmin):
     """Admin interface for model User."""
-    list_display = ('username', 'name', 'email', 'is_staff')
-    fieldsets = (
+    list_display = ('username', 'name', 'email', 'is_staff', 'is_active')
+    list_filter = ('is_active', 'groups')
+    search_fields = ('username', 'name')
+
+    fieldsets = [
         (None, {'fields': ('username', 'name', 'email', 'password')}),
         ('Permissions', {'fields': ('is_active', 'groups')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')})
-    )
-    add_fieldsets = (
+    ]
+    add_fieldsets = [
         (None, {'fields': ('username', 'name', 'email',
                            'password1', 'password2')}),
         ('Permissions', {'fields': ('is_active', 'groups')}),
-    )
+    ]
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super(PhonathonUserAdmin, self).get_fieldsets(
+            request, obj)
+        if not request.user.is_manager_and_above:
+            # hide group permissions from form if user is supervisor and below
+            fieldsets[1] = ('Permissions', {'fields': ('is_active',)})
+        return fieldsets
 
 
 class ProspectAdmin(admin.ModelAdmin):
     """Admin interface for model Prospect."""
     list_display = ('nric', 'name', 'phone_home', 'phone_mobile')
-    fieldsets = (
+    fieldsets = [
         (None, {'fields': ('nric', 'salutation', 'name',
                            'email', 'phone_home', 'phone_mobile')}),
         ('Address', {'fields': ('address_1',
                                 'address_2', 'address_3', 'address_postal')}),
         ('Education', {'fields': ('education_school',
                                   'education_degree', 'education_year')}),
-    )
+    ]
     add_fieldsets = fieldsets
 
 
+# Register models
 admin.site.register(PhonathonUser, PhonathonUserAdmin)
 admin.site.register(Prospect, ProspectAdmin)
 admin.site.register(Pledge)
@@ -48,6 +61,10 @@ admin.site.register(Pool)
 admin.site.register(ResultCode)
 admin.site.register(Call)
 
+# Unregister groups, since groups are automatically populated
+admin.site.unregister(Group)
+
+# Admin site rendering settings
 admin.site.site_header = 'Phonathon Administration'
 admin.site.site_title = admin.site.site_header
 admin.site.index_title = 'Home'
