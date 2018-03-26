240dbe6792b56cbb5996e0aef100a660297a8a96
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2018-03-26 16:04:34 +0800

    Custom managers for Project and ResultCode

--- ccall/models/project.py
+++ ccall/models/project.py
@@ -6,10 +6,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class ProjectManager(models.Manager):
+    """Custom manager for Project."""
+
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
+
+
 class Project(models.Model):
     """Model for a Project."""
+    objects = ProjectManager()
+
     name = models.CharField(
         max_length=50, verbose_name='Project name', unique=True)
 
     def __str__(self):
+        return self.name
+
+    def natural_key(self):
         return self.name
